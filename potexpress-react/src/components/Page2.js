1d0f64c00508c253e9cfa99a7159815c3d0cf6fa
jacobcouvillon <jacob.m.couvillon@gmail.com>
2022-11-26 16:10:59 -0500

    Added iframe map

--- potexpress-react/src/components/Page2.js
+++ potexpress-react/src/components/Page2.js
@@ -5,8 +5,8 @@ export const Page2 = ({onClickBack, savedName, savedStrain, orderNum, savedAddre
     return (
     <div>
 
-        <div> <img src='https://s1.cdn.autoevolution.com/images/news/google-maps-apparently-told-someone-to-drive-into-a-mango-tree-the-internet-goes-nuts-177726_1.jpg'
-        alt = "Map" width="414" height="448"/> </div>
+        {/* <div> <img src='https://s1.cdn.autoevolution.com/images/news/google-maps-apparently-told-someone-to-drive-into-a-mango-tree-the-internet-goes-nuts-177726_1.jpg' */}
+        {/* alt = "Map" width="414" height="448"/> </div> */}
 
         <div> <button onClick={onClickBack} id="backButton">Cancel Order</button> </div>
 
@@ -15,6 +15,13 @@ export const Page2 = ({onClickBack, savedName, savedStrain, orderNum, savedAddre
         <div id="instructions">You can now go for the pickup. Make sure your GPS is on that that we can update the seller</div>
 
         <div id="location">You are currently at {savedAddress}, {savedState}</div>
+
+        <div  className="map-responsive">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13267.364439122515!2d-84.40470066540244!3d33.76477152936022!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1669496934225!5m2!1sen!2sus"
+        width="414" height="448" style={{border:'0'}} loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="map"></iframe>
+        </div>
+
+        {/* &output=embed */}
 
         <div> <img src='https://c8.alamy.com/comp/C2THCY/coconuts-palm-tree-perspective-view-from-floor-high-up-C2THCY.jpg'
         alt = "Map" width="100" height="130" style = {{position:'absolute', left:'20px', top:'670px',}}/> </div>
